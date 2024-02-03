99dd843f2beb6e5012b6ed7d0b9ff45d3263e26d
Bharat <28brana@gmail.com>
2024-02-03 19:46:42 +0530

    feat: morgan chalk

--- server/src/index.js
+++ server/src/index.js
@@ -3,14 +3,16 @@ import cors from "cors";
 import connectDb from "./utils/connect-db.js";
 import routes from "./routes/route.js";
 import handleErrors from "./utils/handleError.js";
+import morgan from 'morgan';
 import { PORT } from "./utils/config.js";
+import { morganChalk } from "./utils/morgan.js";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/", routes);
+app.use(morganChalk);
 app.use(handleErrors);
-
+app.use("/", routes);
 
 const startServer = () => {
   app
