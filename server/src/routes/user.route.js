1592712196205eaf598a82af4e5127f580c824b3
Bharat <28brana@gmail.com>
2024-02-03 14:10:24 +0530

    feat: Joi validation

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { userController } from "../controller/index.js";
+import validate from "../middleware/validate.js";
+import { userValidate } from "../validation/index.js";
 
 const userRouter = Router();
 
-userRouter.post("/login", userController.loginUser);
-userRouter.post("/register", userController.registerUser);
+userRouter.post("/login", validate(userValidate.login),userController.loginUser);
+userRouter.post("/register",validate(userValidate.register), userController.registerUser);
 userRouter.get('/',userController.getAllUser);
 export default userRouter;
